0c4a155d7e2f3005907e8a1d1cecc938f5509449
Gigeone <matthieugh@gmail.com>
2025-11-04 10:24:08 +0100

    initilaization sanity

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,16 @@
-import payment from "@/images/payment.png";
-import Image from "next/image";
 import Container from "./Container";
 
 const Footer = () => {
   return (
     <div className="bg-lightBg text-sm">
       <Container className="py-5">
-        <footer className="flex items-center justify-between">
+        <footer className="flex justify-content items-center justify-around">
           <p className="text-gray-500">
-            Copyright © 2024{" "}
-            <span className="text-darkBlue font-semibold">reactBD</span> all
+            Copyright © 2025{" "}
+            <span className="text-darkBlue font-semibold">MathGH</span> all
             rights reserved.
           </p>
-          <Image src={payment} alt="payment" className="w-64 object-cover" />
+          {/* <Image src={payment} alt="payment" className="w-64 object-cover" /> */}
         </footer>
       </Container>
     </div>
---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,11 @@
+import { type SchemaTypeDefinition } from "sanity";
+
+import { authorType } from "./authorType";
+import { blockContentType } from "./blockContentType";
+import { categoryType } from "./categoryType";
+import { postType } from "./postType";
+import { salesType } from "./saleType";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [blockContentType, categoryType, postType, authorType, salesType],
+};
